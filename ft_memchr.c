bf1296b9c4fbfba38f6f23105df461e7e662c370
Math <46652130+Mmorre@users.noreply.github.com>
2021-01-06 12:10:02 +0100

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorre <mmorre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/06 11:15:28 by mmorre            #+#    #+#             */
+/*   Updated: 2021/01/06 11:40:25 by mmorre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *tmp;
+
+	tmp = (unsigned char*)s;
+	while (n)
+	{
+		if(*tmp == (unsigned char)c)
+			return (tmp);
+		tmp++;
+		n--;
+	}
+	return (NULL);
+}
