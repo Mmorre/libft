35b13a4466f36ad7d052f76b83c76e42e84cc2da
mmorre <math.morre@gmail.com>
2021-01-12 12:59:08 +0100

    2nd commit

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 09:57:12 by math              #+#    #+#             */
-/*   Updated: 2021/01/12 10:26:18 by math             ###   ########.fr       */
+/*   Updated: 2021/01/12 12:36:31 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	
 	i = 0;
 	if (dstsize == 0)
+	{
+		dst[i] = '\0';	
 		return (ft_strlen(src));
+	}
 	while (i < dstsize -1 && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
 	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
 	
 }
